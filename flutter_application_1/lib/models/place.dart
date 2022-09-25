8e18075251f930be9551c4c62e15eee2494cedf8
Luis <luisgmsn2001@gmail.com>
2022-09-25 10:27:46 -0500

    carpeta edit

--- flutter_application_1/lib/models/place.dart
+++
@@ -1,8 +0,0 @@
-class Place {
-  final String title;
-  final String subtitle;
-  final String image;
-  final String description;
-
-  Place(this.title, this.subtitle, this.image, this.description);
-}
